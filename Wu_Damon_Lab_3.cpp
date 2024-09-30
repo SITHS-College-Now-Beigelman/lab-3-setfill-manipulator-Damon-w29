a1858e8c07784ca9bbe90f1147044dc77072e0da
Damon-w29 <Damonw18@nycstudents.net>
2024-09-30 16:08:26 -0400

    actual code

--- Wu_Damon_Lab_3.cpp
+++ Wu_Damon_Lab_3.cpp
@@ -2,3 +2,50 @@
 // Lab 3 
 // Damon Wu 
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+    cout << setw(78) << setfill('#') << "#" << endl; // fills the first 78 spaces with "#"
+  
+    cout << "#" <<setw(77) << setfill (' ') << "#"            << endl; // a hashtag is added first then the next 76 lines are filled with empty spaces then ends off with another hashtag
+
+    cout << "# Ways to access the Task Manager on your Windows computer:" <<setw(19) <<setfill (' ') << "#"  << endl; // a
+
+    cout << "#"  <<setw(77) << setfill (' ') << "#"                                          << endl; 
+
+    cout << "#  Press the key combination Ctrl + Shift + Escape" <<setw(28) <<setfill (' ') << "#"            << endl;
+
+    cout << "#"  <<setw(77) << setfill (' ') << "#"                                          << endl;
+
+    cout << "#   Press the key combination Ctrl + Alt + Delete and select 'Task Manager'" <<setw(3) <<setfill (' ') << "#" << endl;
+
+    cout << "#"  <<setw(77) << setfill (' ') << "#"                                          << endl;
+
+    cout << "#   Type 'Task Manager' in the Windows Start menu search" <<setw(22) <<setfill (' ') << "#"                      << endl;
+
+    cout << "#"  <<setw(77) << setfill (' ') << "#"            << endl; // a hashtag is added first then the next 76 lines are filled with empty spaces then ends off with another hashtag
+
+    cout << setw(78) << setfill('#') << "#" << endl; // fills the first 78 spaces with "#"
+
+// for my own readabilities sake, I added a space in between each line of code. 
+    return 0;
+}
+
+// ##############################################################################
+// #                                                                            #
+// # Ways to access the Task Manager on your Windows computer:                  #
+// #                                                                            #
+// #   Press the key combination Ctrl + Shift + Escape                          #
+// #                                                                            #
+// #   Press the key combination Ctrl + Alt + Delete and select “Task Manager”  #
+// #                                                                            #
+// #   Type “Task Manager” in the Windows Start menu search                     #
+// #                                                                            #
+// ##############################################################################
+
+// [Done] exited with code=0 in 1.604 seconds
