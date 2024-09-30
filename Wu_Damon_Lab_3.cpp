815c646381a4f37cb1036476936639ef3fbe1a78
Damon-w29 <Damonw18@nycstudents.net>
2024-09-30 16:15:45 -0400

    comments and finalizing code

--- Wu_Damon_Lab_3.cpp
+++ Wu_Damon_Lab_3.cpp
@@ -7,28 +7,31 @@
 
 using namespace std;
 
+// setw is a manipulator that allocates a certain amount of spaces to be filled 
+// setfill then fills the amount of spaces allocated by setw with a character of choice, in this case, the hashtag symbol(#), blank spaces could also fill the spaces. 
+
 int main()
 {
 
     cout << setw(78) << setfill('#') << "#" << endl; // fills the first 78 spaces with "#"
   
-    cout << "#" <<setw(77) << setfill (' ') << "#"            << endl; // a hashtag is added first then the next 76 lines are filled with empty spaces then ends off with another hashtag
+    cout << "#" <<setw(77) << setfill (' ') << "#"            << endl;       // a hashtag is added first then the next 76 lines are filled with empty spaces then ends off with another hashtag
 
-    cout << "# Ways to access the Task Manager on your Windows computer:" <<setw(19) <<setfill (' ') << "#"  << endl; // a
+    cout << "# Ways to access the Task Manager on your Windows computer:" <<setw(19) <<setfill (' ') << "#"  << endl; /*after the first line, fill the remainding 18 spaces 
+                                                                                                                     with blanks  and ends off with a # */
+    cout << "#"  <<setw(77) << setfill (' ') << "#"                                          << endl; // starts with a # then fills 76 spaces blank with it ending in a #
 
-    cout << "#"  <<setw(77) << setfill (' ') << "#"                                          << endl; 
+    cout << "#  Press the key combination Ctrl + Shift + Escape" <<setw(28) <<setfill (' ') << "#"            << endl; // after the text, fill 27 spaces blank then ends with a #
 
-    cout << "#  Press the key combination Ctrl + Shift + Escape" <<setw(28) <<setfill (' ') << "#"            << endl;
+    cout << "#"  <<setw(77) << setfill (' ') << "#"                                          << endl; // starts with a # then fills 76 spaces blank with it ending in a #
 
-    cout << "#"  <<setw(77) << setfill (' ') << "#"                                          << endl;
+    cout << "#   Press the key combination Ctrl + Alt + Delete and select 'Task Manager'" <<setw(3) <<setfill (' ') << "#" << endl; // after the text, fill 2 spaces blank then ends with a #
 
-    cout << "#   Press the key combination Ctrl + Alt + Delete and select 'Task Manager'" <<setw(3) <<setfill (' ') << "#" << endl;
+    cout << "#"  <<setw(77) << setfill (' ') << "#"                                          << endl; // starts with a # then fills 76 spaces blank with it ending in a #
 
-    cout << "#"  <<setw(77) << setfill (' ') << "#"                                          << endl;
+    cout << "#   Type 'Task Manager' in the Windows Start menu search" <<setw(22) <<setfill (' ') << "#"       << endl; // after the text, fill 21 spaces blank then ends with a #
 
-    cout << "#   Type 'Task Manager' in the Windows Start menu search" <<setw(22) <<setfill (' ') << "#"                      << endl;
-
-    cout << "#"  <<setw(77) << setfill (' ') << "#"            << endl; // a hashtag is added first then the next 76 lines are filled with empty spaces then ends off with another hashtag
+    cout << "#"  <<setw(77) << setfill (' ') << "#"            << endl;        // a hashtag is added first then the next 76 lines are filled with empty spaces then ends off with another hashtag
 
     cout << setw(78) << setfill('#') << "#" << endl; // fills the first 78 spaces with "#"
 
